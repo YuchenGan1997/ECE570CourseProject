c8053bf6f950fb1d395bbfe540866ecfff21d0d5
甘雨琛 <ganyuchen@ganyuchendeMacBook-Pro.local>
2022-04-22 20:39:27 -0400

    ECE5710Version1.0

---
+++ python-microservices/admin/products/producer.py
@@ -0,0 +1,12 @@
+import pika, json
+
+params = pika.URLParameters('amqps://ztegcipp:PrXdKG9BvBL9pUe91q5kVvxh-e1lMux-@shark.rmq.cloudamqp.com/ztegcipp')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
