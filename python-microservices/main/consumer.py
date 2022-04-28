f592ba6c3bc13483d19d4e7354dc7502d55f8ff2
甘雨琛 <ganyuchen@ganyuchendeMacBook-Pro.local>
2022-04-28 17:30:02 -0400

    Fix Delete and Edit Bug

--- python-microservices/main/consumer.py
+++ python-microservices/main/consumer.py
@@ -23,14 +23,16 @@ def callback(ch, method, properties, body):
         print('Product Created')
 
     elif properties.content_type == 'product_updated':
-        product = Product.query.get(data['id'])
+        #product = Product.query.get(data['id'])
+        product = Product.query.filter_by(id=data['id']).first()
         product.title = data['title']
         product.image = data['image']
         db.session.commit()
         print('Product Updated')
 
     elif properties.content_type == 'product_deleted':
-        product = Product.query.get(data)
+        # product = Product.query.get(data)
+        product = Product.query.filter_by(id=data).first()
         db.session.delete(product)
         db.session.commit()
         print('Product Deleted')
